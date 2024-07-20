1a13854ac7b8cdb65c69a1dc31b52f20da778001
Akhil244 <akhil244392@gmail.com>
2024-07-20 23:37:35 +0530

    Implement auth,Appwrite,sentry

---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://c1f6f8ab152f0cf71b9ef8e65cc09ba1@o4507635380584448.ingest.us.sentry.io/4507635383140352",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+  
+});
